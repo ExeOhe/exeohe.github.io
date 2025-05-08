021eef91725b6a3cb50643f41a0bc865342be18d
Jecob Snyder <x0jxc0b315@gmail.com>
2025-05-08 18:30:24 -0400

    prepared 'feeling_response'func. for reusability and logic isolation, testability, scalability

--- backend/main.py
+++ backend/main.py
@@ -2,12 +2,12 @@
 goodfeel = ["good", "excited", "great", "happy", "fantastic", "amazing"]
 badfeel = ["bad", "sad", "upset", "angry","tired","exausted","depressed","mad"]
 
-feeling= input("how are you feeling?")
-for word in goodfeel:
-    if word in feeling:
-     print("Hurray ! Im stoked that youre feeling", feeling)
-else:
-    for word in badfeel:
-     if word in feeling:
-      print("I'm so sorry you're going through this", feeling)
-     break
+def feeling_response(feeling: str):
+    for word in goodfeel:
+        if word in feeling:
+        print("Hurray ! Im stoked that youre feeling", feeling)
+    else:
+        for word in badfeel:
+        if word in feeling:
+        print("I'm so sorry you're going through this", feeling)
+        
